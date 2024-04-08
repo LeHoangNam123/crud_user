3015764344dcdd1f6d0f0d5e9c38ad13733bd7e3
LeHoangNam123 <lehoangnam16570@gmail.com>
2024-04-08 13:56:50 +0700

    Update

--- myproject/app/Http/Controllers/UserControllers.php
+++ myproject/app/Http/Controllers/UserControllers.php
@@ -13,45 +13,10 @@ use Illuminate\Support\Facades\DB;
 
 class UserControllers extends Controller
 {
-    /**
-     * View
-     */
     public function show($user_id){
         $user = User::find($user_id);
         return view('show_user', compact('user'));
     }
-
-    /**
-     * Để làm chức năng đăng ký cần 2 bước
-     * B1: giao diện -> tạo hàm r -> route
-     * B2: xử lý $request 
-     */
-    public function register(){
-        return view('auth/register'); //hàm này trả về giao diện B1
-    }
-
-    public function customRegister(Request $request){
-        /**
-         * Nhận request -> valitate data 
-         * Xử lý kiểm tra và thêm vào db
-         */          
-        $validatedData = $request->validate([
-            // 'tên trường dữ liệu' => 'xử lý ngoại lệ'
-            'name' => 'required|string|max:255',
-            'phone' => 'required|string|max:20',
-            'image' => 'nullable|image|max:2048',
-            'email' => 'required|string|email|unique:users|max:255',
-            'password' => 'required|string|min:8',
-        ]);
-        $user = new User([
-            'name' => $validatedData['name'],
-            'phone' => $validatedData['phone'],
-            'email' => $validatedData['email'],
-            'password' => Hash::make($validatedData['password']), // mã hóa mật khẩu 
-        ]);
-
-        //gán image
-    
     /**
      * Cập nhật người dùng cần 2 bước
      * B1: trả về trang sửa người dùng + người dùng muốn sửav thông qua  compact
@@ -90,11 +55,18 @@ class UserControllers extends Controller
             $image = $request->file('image');
             $filename = 'user'. '-'.time().rand(1,999). '.' .$image->extension();
             $image->move(public_path('images/users'), $filename);
-            $user->image = $filename; //gán tên ảnh cho trường avatar của user
+            $user->image = $filename;
         }
-        //lưu user vào db
         $user->save();
-        return redirect('login');
+        // return redirect()->route('index')->with("success", "Cập nhật user thành công");
+        return redirect()->route('login');
+    }
+
+    public function index(){
+        $users = DB::table('users')->paginate(4);
+        return view('index', compact('users'));
+    }
+    //
     public function delete($user_id){
         /**
          * Tìm coi user đó tồn tại không theo user_id
@@ -180,17 +152,5 @@ class UserControllers extends Controller
          */ 
         Auth::logout();
         return redirect()->route('login');
-    // list danh sach va phan trang
-    public function index(){
-        $users = DB::table('users')->paginate(4);
-        return view('index', compact('users'));
     }
-            $user->image = $filename;
-        }
-        $user->save();
-        // return redirect()->route('index')->with("success", "Cập nhật user thành công");
-        return redirect()->route('login');
-    }
-
-    
 }
