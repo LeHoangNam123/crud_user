ad198aad288554ece20f4f1f3295cadf41beaf17
LeHoangNam123 <lehoangnam16570@gmail.com>
2024-04-08 13:56:37 +0700

    Update

--- myproject/app/Http/Controllers/UserControllers.php
+++ myproject/app/Http/Controllers/UserControllers.php
@@ -129,6 +129,48 @@ class UserControllers extends Controller
             return back()->with("error", "Không mật khẩu không chính xác!");
         }
     }
+
+    /**
+     * Để làm chức năng đăng ký cần 2 bước
+     * B1: giao diện -> tạo hàm r -> route
+     * B2: xử lý $request 
+     */
+    public function register(){
+        return view('auth/register'); //hàm này trả về giao diện B1
+    }
+
+    public function customRegister(Request $request){
+        /**
+         * Nhận request -> valitate data 
+         * Xử lý kiểm tra và thêm vào db
+         */
+        $validatedData = $request->validate([
+            // 'tên trường dữ liệu' => 'xử lý ngoại lệ'
+            'name' => 'required|string|max:255',
+            'phone' => 'required|string|max:20',
+            'image' => 'nullable|image|max:2048',
+            'email' => 'required|string|email|unique:users|max:255',
+            'password' => 'required|string|min:8',
+        ]);
+        $user = new User([
+            'name' => $validatedData['name'],
+            'phone' => $validatedData['phone'],
+            'email' => $validatedData['email'],
+            'password' => Hash::make($validatedData['password']), // mã hóa mật khẩu 
+        ]);
+
+        //gán image
+        if ($request->hasFile('image')) { // kiểm ta coi có request này k
+            $image = $request->file('image');
+            $filename = 'user'. '-'.time().rand(1,999). '.' .$image->extension();
+            $image->move(public_path('images/users'), $filename);
+            $user->image = $filename; //gán tên ảnh cho trường avatar của user
+        }
+        //lưu user vào db
+        $user->save();
+        return redirect('login');
+    }
+
     public function logout(){
         // hàm đăng xuất 
         /**
